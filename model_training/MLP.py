508f981d06e23f164b0441f30fbb12e685e1a363
LukeRaw <luke.rawlence@gmail.com>
2022-08-03 12:09:50 +0100

    current experimental code

--- model_training/MLP.py
+++ model_training/MLP.py
@@ -29,7 +29,7 @@ y = np.load("/home/luke/code/molecular-magic/auto_bandwidth_features/labels.npy"
 
 # TO-DO: Incorperate MolE8 train test split logic
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=random_seed)
+    X, y, test_size=0.2, random_state=random_seed)
 
 ###################################################
 # Experimental setup                              #
@@ -41,8 +41,8 @@ epochs = 500
 
 # Define learning rate schedule
 lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-    boundaries=[579 * 20, 579 * 100, 579 * 400],
-    values=[1e-3, 1e-4, 1e-5, 1e-6],
+    boundaries=[691 * 10, 691 * 30, 691 * 60, 691 * 400],
+    values=[1e-2, 1e-3, 1e-4, 1e-5, 1e-6],
     name="lr_decay",
 )
 
@@ -52,9 +52,9 @@ def objective(trial: op.Trial):
     # Define model architecture
     model = keras.Sequential()
     model.add(keras.layers.Input(shape=(len(X[0]))))
-    for i in range(trial.suggest_int("num_layers", 1, 3)):
+    for i in range(trial.suggest_int("num_layers", 2, 5)):
         model.add(keras.layers.Dense(
-            units=trial.suggest_categorical(f"l{i}_dims", [16, 64, 128]),
+            units=trial.suggest_categorical(f"l{i}_dims", [64, 128, 256]),
             activation="relu")
             )
     model.add(keras.layers.Dense(units=1, activation="linear"))
@@ -76,22 +76,22 @@ def objective(trial: op.Trial):
     print("Compiled model")
 
     # Configure the weights and biases experiment
-    # wandb_config = dict(
-    #     **{
-    #         "trial_number": trial.number,
-    #         "learning_rate": "20,40,400,1600,2800 st 1e-2",
-    #         "batch_size": batch_size,
-    #         "n_layers": trial.params["num_layers"],
-    #     },
-    #     **trial.params,
-    # )  # Automatically includes the parameters we are using
-    # wandb.init(
-    #     reinit=True,
-    #     project="MolecularMagic",
-    #     entity="molecular-magicians",
-    #     group=exp_name,
-    #     config=wandb_config,
-    # )
+    wandb_config = dict(
+        **{
+            "trial_number": trial.number,
+            "learning_rate": "20,40,400,1600,2800 st 1e-2",
+            "batch_size": batch_size,
+            "n_layers": trial.params["num_layers"],
+        },
+        **trial.params,
+    )  # Automatically includes the parameters we are using
+    wandb.init(
+        reinit=True,
+        project="MolecularMagic",
+        entity="molecular-magicians",
+        group=exp_name,
+        config=wandb_config,
+    )
 
     # Fit the model
     hist = model.fit(
@@ -101,14 +101,14 @@ def objective(trial: op.Trial):
         epochs=epochs,
         callbacks=[
             op.integration.TFKerasPruningCallback(trial, "val_loss"),
-            # WandbCallback(monitor="val_loss", save_model=False, log_weights=True),
+            WandbCallback(monitor="val_loss", save_model=False, log_weights=True),
             # Consider early stopping callback
         ],
         validation_data=(X_test, y_test),
     )
 
     # Mark the experiment as ended
-    # wandb.finish(quiet=True)
+    wandb.finish(quiet=True)
 
     return hist.history["val_loss"][-1]  # Return the end validation loss
 
