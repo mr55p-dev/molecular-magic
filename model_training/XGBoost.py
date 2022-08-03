a844f16beb03283d245714b2ce6af4793e37363b
LukeRaw <luke.rawlence@gmail.com>
2022-08-03 16:24:36 +0100

    updated scripts

--- model_training/XGBoost.py
+++ model_training/XGBoost.py
@@ -1,6 +1,6 @@
 from distutils.log import Log
 import numpy as np
-from sklearn.linear_model import RidgeCV
+import 
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -13,9 +13,9 @@ y = np.load("/home/luke/code/molecular-magic/auto_bandwidth_features/labels.npy"
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=random_seed)
 
-reg = RidgeCV()
+reg = Ridge()
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 
-print(mae) # Achieves a MAE of 4.206
+print(mae)
