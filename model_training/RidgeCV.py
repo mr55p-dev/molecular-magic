0b3d0d65690bc01256c8fc1149bc8f599aada20a
LukeRaw <luke.rawlence@gmail.com>
2022-08-10 15:12:09 +0100

    updated code with soichiometric split

--- model_training/RidgeCV.py
+++ model_training/RidgeCV.py
@@ -2,20 +2,22 @@ from distutils.log import Log
 import numpy as np
 from sklearn.linear_model import RidgeCV
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
+from magic.split import stoichiometric_split
 
 random_seed = 50
 
-X = np.load("/home/luke/code/molecular-magic/auto_bandwidth_features/features.npy")
-y = np.load("/home/luke/code/molecular-magic/auto_bandwidth_features/labels.npy").astype(np.double)
+X = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/features.npy")
+y = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/labels.npy").astype(np.double)
 
 # TO-DO: Incorperate MolE8 train test split logic
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=random_seed)
+X_train, X_test, y_train, y_test = stoichiometric_split(
+    X, y, random_state=random_seed)
 
 reg = RidgeCV()
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 
-print(mae) # Achieves a MAE of 4.206
+print(mae) # Achieves a MAE of 4.336 (using stoichiometric split)
+
+# Achieves a MAE of 3.381 (using new features)
