ae0e9511a861ba1a5ce58150d92388ec45278dda
LukeRaw <luke.rawlence@gmail.com>
2022-08-12 11:46:26 +0100

    Concluded preliminary experiments

---
+++ model_training/archive/AdaBoostRegressor.py
@@ -0,0 +1,26 @@
+from distutils.log import Log
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from magic.split import stoichiometric_split
+# from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import AdaBoostRegressor
+
+random_seed = 50
+
+# We are focusing on the prediction of free energy (this is the target)
+X = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/features.npy")
+y = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/labels.npy").astype(np.double)
+
+X_train, X_test, y_train, y_test = stoichiometric_split(
+    X, y, random_state=random_seed)
+
+
+
+reg = AdaBoostRegressor(base_estimator=Ridge(), random_state=random_seed)
+reg.fit(X_train, y_train)
+y_pred = reg.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+
+print(mae) # Achieves a MAE of 4.79
--- model_training/archive/XGBoost.py
+++ model_training/archive/XGBoost.py
@@ -1,23 +1,23 @@
 from distutils.log import Log
 import numpy as np
-from sklearn.linear_model import RidgeCV
 from sklearn.metrics import mean_absolute_error
 from magic.split import stoichiometric_split
+# from sklearn.model_selection import train_test_split
+
+import xgboost as xgb
 
 random_seed = 50
 
+# We are focusing on the prediction of free energy (this is the target)
 X = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/features.npy")
 y = np.load("/home/luke/code/molecular-magic/autoband_badh_freeeng/labels.npy").astype(np.double)
 
-# TO-DO: Incorperate MolE8 train test split logic
 X_train, X_test, y_train, y_test = stoichiometric_split(
     X, y, random_state=random_seed)
 
-reg = RidgeCV()
+reg = xgb.XGBRegressor()
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 
-print(mae) # Achieves a MAE of 4.336 (using stoichiometric split)
-
-# Achieves a MAE of 3.381 (using new features)
+print(mae)
