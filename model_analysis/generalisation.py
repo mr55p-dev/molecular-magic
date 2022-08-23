03f5b4a976eac090f389d538c6e003d34922189e
Ellis Lunnon <ellislunnon@gmail.com>
2022-08-23 21:54:05 +0100

    Include the identity mask to get mols

--- model_analysis/generalisation.py
+++ model_analysis/generalisation.py
@@ -14,6 +14,7 @@ model = get_model_artifact(...)
 evaluation_path = get_vector_artifact(...)
 X_eval = np.load(evaluation_path / "features.npy")
 y_eval = np.load(evaluation_path / "labels.npy")
+idx_eval = np.load(evaluation_path / "identities.npy")
 
 # Load the dataset which produced those vectors
 dataset_path = get_vector_parent(...)
@@ -21,8 +22,14 @@ dataset_path = get_vector_parent(...)
 
 # Run inference
 y_pred = model.predict(X_eval)
+y_err = np.abs(y_pred - y_eval)
 
 # Read structures from the sdf archive
 mols = read_sdf_archive(dataset_path / "archive.sdf.bz2")
+
+# Get a mask of molecules with error > threshold kcal/mol
+threshold = 10.0
+large_err_mol_ids = idx_eval[y_err > threshold]
+large_err_mols = filter(lambda x: x.data["id"] in large_err_mol_ids, mols)
 
 # Analyse how errors are distributed (separate script)
