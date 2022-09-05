ff7b21c30bafdf3dae483204c3d678bf8faa9f6f
Ellis Lunnon <ellislunnon@gmail.com>
2022-09-05 19:43:48 +0100

    Finished sweep definition

--- model_training/sweep_kde_bandwidth.py
+++ model_training/sweep_kde_bandwidth.py
@@ -14,6 +14,11 @@ from molmagic.aggregator import autobin_mols
 run = ml.run_controller.use_run()
 run_config = run.config
 
+# Setup the vector directory
+vector_dir = Path("/tmp/vectors/")
+if vector_dir.exists():
+    rmtree(vector_dir) if vector_dir.is_dir() else vector_dir.unlink()
+
 # Set the configuration options
 seed = 50
 split_type = "random"
@@ -32,33 +37,30 @@ bond_bandwidth = run_config["bond-bandwidth"]
 angle_bandwidth = run_config["angle-bandwidth"]
 cfg.aggregation["bond-bandwidth"] = bond_bandwidth
 cfg.aggregation["angle-bandwidth"] = angle_bandwidth
+cfg.plotting["save-dir"] = vector_dir
 
 dataset_base = run_config["dataset_base"]
 dataset_version = run_config["dataset_version"]
 dataset_dir = ml.get_filtered_artifact(f"{dataset_base}:{dataset_version}")
 
 # Parse the filtered base dataset
-molecules = read_sdf_archive(dataset_dir)
+molecules = list(read_sdf_archive(dataset_dir))
 
 # Generate the dataset
-molecule_data = map(calculate_mol_data, molecules)
-features, labels_raw, metadata = autobin_mols(molecule_data, plot_histograms=False)
+molecule_data = [calculate_mol_data(molecule) for molecule in molecules]
+features, labels_raw, metadata = autobin_mols(molecule_data, plot_histograms=True)
 identities = np.array([mol.data["id"] for mol in molecules]).astype(np.int32)
 
 # Save the dataset artifact
-vector_dir = Path("/tmp/vectors/")
-if vector_dir.isdir():
-    rmtree(vector_dir)
-
-vector_dir.mkdir(exist_ok=False)
+vector_dir.mkdir(exist_ok=True)
 np.save(vector_dir / "features.npy", features)
 np.save(vector_dir / "labels.npy", labels_raw)
 np.save(vector_dir / "identities.npy", identities)
 with (vector_dir / "metadata.yml").open("w") as metadata_file:
     yaml.dump(metadata, metadata_file)
 
-artifact_name = dataset_base + f"-{str(bandwidth)}"
-ml.log_vector_artifact(artifact_name, features, vector_dir, plot_hisrograms=False)
+artifact_name = dataset_base + f"-{str(bond_bandwidth)}-{str(angle_bandwidth)}"
+ml.log_vector_artifact(artifact_name, features, vector_dir, plot_histograms=True)
 
 # Create train test split
 labels = ml.get_label_type(labels_raw, target)
@@ -68,14 +70,15 @@ X_train, X_test, y_train, y_test = splitter(features, labels, random_state=seed)
 # Run the experiment
 model_base = RidgeCV()
 model = model_base.fit(X_train, y_train)
-y_pred = model.predict(X_test, y_test)
+y_pred = model.predict(X_test)
 
 # Evaluate the predictions
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
+val_mse = mean_squared_error(y_test, y_pred)
+val_mae = mean_absolute_error(y_test, y_pred)
 
 # Save the model
 ml.log_sklearn_model(model)
 
 # Log the results
-run.log({"mean_squared_error": mse, "mean_absolute_error": mae})
+# props = {"mse": mse, "mae": mae, "val_mse": val_mse, "val_mae": val_mae}
+run.log({"val_mse": val_mse, "val_mae": val_mae})
