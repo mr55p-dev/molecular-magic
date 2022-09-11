dd530c705d20a6c7733251fa16b677b60d0e5e21
Ellis Lunnon <ellislunnon@gmail.com>
2022-09-11 12:28:58 +0100

    Changed the sweep run update script again

--- scripts/update-sweep-runs.py
+++ scripts/update-sweep-runs.py
@@ -8,17 +8,20 @@ from tqdm import tqdm
 
 # Get all the associated runs
 api = wandb.Api()
-sweep = api.sweep("molecular-magicians/MolecularMagic/hqb9gzxa")
+# runs = api.runs(filters={"tags": {"$in": ["Neural-Network"]}})
+sweep = api.sweep("molecular-magicians/MolecularMagic/9iguqpio")
 
 for run in tqdm(sweep.runs):
     # Get the generated vector dataset
-    artifact = get_artifact_of_kind(
-        run.entity + "/" + run.project + "/" + run.id, "model"
-    )
-    artifact_path = Path(artifact.download())
-    model = artifact_path / "model" / "model"
-    with model.open("rb") as f:
-        m = p.loads(f.read())
-    run.summary.update({"alpha": m.alpha_})
+    # artifact = get_artifact_of_kind(
+    #     run.entity + "/" + run.project + "/" + run.id, "model"
+    # )
+    # artifact_path = Path(artifact.download())
+    # model = artifact_path / "model" / "model"
+    # with model.open("rb") as f:
+    #     m = p.loads(f.read())
+    # run.summary.update({"alpha": m.alpha_})
+    # run.update()
+    # rmtree(artifact_path)
+    run.summary.update({"best_val_mae": run.history()["val_mae"].min()})
     run.update()
-    rmtree(artifact_path)
