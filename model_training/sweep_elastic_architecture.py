ce5d35a0224b75938952d9d89a73d4e91e373c63
Ellis Lunnon <ellislunnon@gmail.com>
2022-10-03 08:27:58 +0100

    Added model sweep configurations and scripts.

--- model_training/sweep_elastic_architecture.py
+++ model_training/sweep_elastic_architecture.py
@@ -5,7 +5,7 @@ import wandb
 from molmagic import ml
 from molmagic.ml import run_controller
 from sklearn.linear_model import ElasticNet
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 
 
 """Parameters to sweep
@@ -20,15 +20,13 @@ The major bottleneck for most cases will be transforming the molecules into the
 aggregated representation
 """
 
-# SKlearn setup
-random_seed = 50
-
 # WandB setup (populated due to the sweep)
 run = wandb.init()
 run_controller.set_run(run)
-run.config.update({"algorithm": "Elastic", "seed": random_seed})
+run.config.update({"algorithm": "Elastic"})
 
 # Experimental setup
+seed = run.config["seed"]
 split_type = run.config["splitting_type"]
 label_type = run.config["label_name"]
 training_artifact = run.config["training_artifact"]
@@ -44,10 +42,10 @@ y_raw = np.load(basepath / "labels.npy").astype(np.double)
 y = ml.get_label_type(y_raw, label_type)
 
 splitter = ml.get_split(split_type)
-X_train, X_test, y_train, y_test = splitter(X, y, random_state=random_seed)
+X_train, X_test, y_train, y_test = splitter(X, y, random_state=seed)
 
 # Define the model
-model = ElasticNet(random_state=random_seed, max_iter=max_iter, l1_ratio=l1_ratio, alpha=alpha)
+model = ElasticNet(random_state=seed, max_iter=max_iter, l1_ratio=l1_ratio, alpha=alpha)
 
 # Fit the model
 fitted_model = model.fit(X_train, y_train.squeeze())
@@ -59,17 +57,18 @@ ml.log_sklearn_model(fitted_model)
 y_pred = model.predict(X_test).squeeze()
 val_mse = mean_squared_error(y_test.squeeze(), y_pred)
 val_mae = mean_absolute_error(y_test.squeeze(), y_pred)
+val_mape = mean_absolute_percentage_error(y_test.squeeze(), y_pred)
 
-absolute_err = np.abs(y_test.squeeze() - y_pred).reshape(-1, 1)
-err_table = wandb.Table(data=pd.DataFrame(absolute_err, columns=["Absolute error"]))
+data = np.concatenate((y_test.reshape(-1, 1), y_pred.reshape(-1, 1)), axis=1)
+errors = pd.DataFrame(data, columns=["label", "prediction"])
+
+err_table = wandb.Table(data=errors)
 
 wandb.log(
     {
         "val_mae": val_mae,
         "val_mse": val_mse,
+        "val_mape": val_mape,
         "Error table": err_table,
-        "Error histogram": wandb.plot.histogram(
-            err_table, "Absolute error", "Absolute error distribution"
-        ),
     }
 )
